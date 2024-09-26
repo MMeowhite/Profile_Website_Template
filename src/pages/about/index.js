3bb95f5a2da7c1b29b7169e3466021d469a5de03
Baimiaomiao123 <18723560427@163.com>
2024-09-27 01:24:14 +0800

    first commit

---
+++ src/pages/about/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+    <section id="about">
+        <h1>About Me</h1>
+        <p>I am a bioinformatics enthusiast with a focus on simplifying complex concepts...</p>
+    </section>
+);
+
+export default About;
---
+++ src/pages/research/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Projects = () => (
+    <section id="projects">
+        <h1>Projects</h1>
+        <ul>
+            <li>Research project1: <a href="#">GitHub Repo</a></li>
+            <li>Research project2: <a href="#">GitHub Repo</a></li>
+        </ul>
+    </section>
+);
+
+export default Projects;
