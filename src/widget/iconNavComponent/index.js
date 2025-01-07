a89aba7f32bd277a7f3ef2f4117a61248e1923b3
Baimiaomiao123 <18723560427@163.com>
2025-01-07 22:15:40 +0800

    main page

--- src/widget/iconNavComponent/index.js
+++ src/widget/iconNavComponent/index.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import styles from './iconNavComponent.module.css';
-import {Link} from "react-router-dom"; // 引入模块化样式
+import { Link } from "react-router-dom";
+import { useTheme } from "../../components/themeProvider";
 
 const IconNavComponent = () => {
     const [linkIconObj, setLinkIcon] = useState([]);
+    const { isDarkMode } = useTheme(); // 修改为 isDarkMode
 
     useEffect(() => {
         const fetchConfig = async () => {
@@ -21,17 +23,25 @@ const IconNavComponent = () => {
         fetchConfig();
     }, []);
 
+    // 动态获取当前主题的图标填充颜色
+    // const getFillColor = () => (isDarkMode ? "#ffffff" : "#000000"); // 修改为 isDarkMode
+
     return (
         <nav className={styles.nav}>
             <ul className={styles.navList}>
                 {linkIconObj.map((item, index) => (
                     <li key={index} className={styles.navItem}>
                         <Link to={item.link} className={styles.navLink}>
-                            <img
-                                src={item.icon}
-                                alt={item.type}
-                                className={styles.iconStyle}
-                            />
+                            {item.icon.endsWith('.svg') ? (
+                                <img
+                                    src={item.icon}
+                                    alt={item.type}
+                                    className={styles.iconStyle}
+                                    style={{ filter: isDarkMode ? "invert(1)" : "invert(0)" }} // 使用反转颜色
+                                />
+                            ) : (
+                                <img src={item.icon} alt={item.type} className={styles.iconStyle} />
+                            )}
                         </Link>
                     </li>
                 ))}
