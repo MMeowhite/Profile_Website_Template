4e51fde014b8c91748829dfc37f57f689645acae
Baimiaomiao123 <18723560427@163.com>
2025-02-05 20:30:09 +0800

    experience page css style

--- src/pages/Experience/index.js
+++ src/pages/Experience/index.js
@@ -1,129 +1,146 @@
-import React from 'react'
-import {useTheme} from "../../components/themeProvider";
+import React from 'react';
+import { useTheme } from "../../components/themeProvider";
 import useConfig from "../../utils/useConfig";
-
+import { Container, Row, Col, Card, ListGroup, Button } from 'react-bootstrap';
 
 const Experience = () => {
-    const { isDarkMode } = useTheme()
-
-    const {configValue:experienceData, error, loading} = useConfig('pages.experience')
+    const { isDarkMode } = useTheme();
+    const { configValue: experienceData, error, loading } = useConfig('pages.experience');
 
     if (loading) {
-        return (<div>Loading Experience Data...</div>)
+        return (<div style={{ textAlign: "center", marginTop: "50px", fontSize: "18px" }}>Loading Experience Data...</div>);
     }
 
     if (error) {
-        return (<div>Error : ${error}</div>)
+        return (<div style={{ textAlign: "center", marginTop: "50px", fontSize: "18px" }}>Error: {error}</div>);
     }
 
     const { education, internshipsAndVolunteering, researchProjects, skills, awardsAndHonors } = experienceData;
 
     return (
-        <div id="experience-page" style={{background: "inherit", marginTop: "110px"}}>
-            <h1>Experience</h1>
-            <section>
-                <h2>Education</h2>
-                {education.map((edu, index) => (
-                    <div key={index} style={{ marginBottom: "20px" }}>
-                        <a href={edu.university.url} target="_blank" rel="noopener noreferrer">
-                            {edu.university.logo ? edu.university.logo : ""}
-                            <h3>{edu.university.name}</h3>
-                        </a>
-                        <p><strong>Time:</strong>{edu.time} &nbsp;&nbsp;&nbsp;&nbsp; <strong>GPA:</strong> {edu.GPA}</p>
-                        <p><strong>Major:</strong> {edu.major}</p>
-                        <p><strong>Main Course:</strong> {edu.course}</p>
-                    </div>
-                ))}
-            </section>
+        <div id="experience-page" style={{ background: "inherit", marginTop: "110px" }}>
+            <Container>
+                <h1 className="my-4">Experience</h1>
 
-            <section>
-                <h2>Internships & Volunteering</h2>
-                {internshipsAndVolunteering.map((internship, index) => (
-                    <div key={index} style={{ marginBottom: "20px" }}>
-                        <a href={internship.location.url ? internship.location.url : ""} target="_blank" rel="noopener noreferrer">
-                            {internship.location.logo ? internship.location.logo : ""}
-                            <h3>{internship.location.name}</h3>
-                        </a>
-                        <p>{internship.time}</p>
-                        <p>{internship.work}</p>
-                    </div>
-                ))}
-            </section>
+                {/* Education Section */}
+                <section>
+                    <h2>Education</h2>
+                    {education.map((edu, index) => (
+                        <Card key={index} className="mb-4">
+                            <Card.Body>
+                                <a href={edu.university.url} target="_blank" rel="noopener noreferrer">
+                                    {edu.university.logo && <img src={edu.university.logo} alt="University Logo" style={{ width: "50px", marginRight: "10px" }} />}
+                                    <Card.Title>{edu.university.name}</Card.Title>
+                                </a>
+                                <Card.Text><strong>Time:</strong> {edu.time} &nbsp;&nbsp;&nbsp;&nbsp; <strong>GPA:</strong> {edu.GPA}</Card.Text>
+                                <Card.Text><strong>Major:</strong> {edu.major}</Card.Text>
+                                <Card.Text><strong>Main Course:</strong> {edu.course}</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    ))}
+                </section>
 
-            <section>
-                <h2>Research Projects</h2>
-                {researchProjects.map((project, index) => (
-                    <div key={index} style={{ marginBottom: "20px" }}>
-                        <a href={project.location.url} target="_blank" rel="noopener noreferrer">
-                            {project.location.logo ? project.location.logo : ""}
-                            <h3>{project.location.name}</h3>
-                        </a>
-                        <p>{project.time}</p>
-                        <p><strong>Director:</strong> {Array.isArray(project.directors) ? project.directors.map((director, index) => (
-                            <a key={index} href={director.link} target="_blank" rel="noopener noreferrer">
-                                {director.name}
-                            </a>
-                        )) : null}</p>
-                        <p>{project.work}</p>
-                    </div>
-                ))}
-            </section>
+                {/* Internships & Volunteering Section */}
+                <section>
+                    <h2>Internships & Volunteering</h2>
+                    {internshipsAndVolunteering.map((internship, index) => (
+                        <Card key={index} className="mb-4">
+                            <Card.Body>
+                                <a href={internship.location.url ? internship.location.url : ""} target="_blank" rel="noopener noreferrer">
+                                    {internship.location.logo && <img src={internship.location.logo} alt="Internship Logo" style={{ width: "50px", marginRight: "10px" }} />}
+                                    <Card.Title>{internship.location.name}</Card.Title>
+                                </a>
+                                <Card.Text>{internship.time}</Card.Text>
+                                <Card.Text>{internship.work}</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    ))}
+                </section>
 
-            <section>
-                <h2>Skills</h2>
-                {skills.map((skill, skillIndex) => (
-                    <div key={skillIndex} style={{ marginBottom: "20px" }}>
-                        <h4>Field: {skill.field}</h4>
-                        {Object.entries(skill.list).map(([key, value], index) => (
-                            Array.isArray(value) ? (
-                                <div key={index}>
-                                    <strong>{key}:</strong>
-                                    <ul>
-                                        {value.map((item, itemIndex) => (
-                                            <li key={itemIndex}>{item}</li>
-                                        ))}
-                                    </ul>
-                                </div>
-                            ) : (typeof value === 'object' ? (
-                                <div key={index}>
-                                    <strong>{key}:</strong>
-                                    {Object.entries(value).map(([subKey, subValue], subIndex) => (
-                                        <div key={subIndex}>
-                                            <strong>{subKey}:</strong>
-                                            <ul>
-                                                {Array.isArray(subValue) ? (
-                                                    subValue.map((subItem, subItemIndex) => (
-                                                        <li key={subItemIndex}>{subItem}</li>
-                                                    ))
-                                                ) : (
-                                                    <li>{subValue}</li>
-                                                )}
-                                            </ul>
+                {/* Research Projects Section */}
+                <section>
+                    <h2>Research Projects</h2>
+                    {researchProjects.map((project, index) => (
+                        <Card key={index} className="mb-4">
+                            <Card.Body>
+                                <a href={project.location.url} target="_blank" rel="noopener noreferrer">
+                                    {project.location.logo && <img src={project.location.logo} alt="Research Logo" style={{ width: "50px", marginRight: "10px" }} />}
+                                    <Card.Title>{project.location.name}</Card.Title>
+                                </a>
+                                <Card.Text>{project.time}</Card.Text>
+                                <Card.Text><strong>Director:</strong> {Array.isArray(project.directors) ? project.directors.map((director, index) => (
+                                    <a key={index} href={director.link} target="_blank" rel="noopener noreferrer">
+                                        {director.name}
+                                    </a>
+                                )) : null}</Card.Text>
+                                <Card.Text>{project.work}</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    ))}
+                </section>
+
+                {/* Skills Section */}
+                <section>
+                    <h2>Skills</h2>
+                    {skills.map((skill, skillIndex) => (
+                        <Card key={skillIndex} className="mb-4">
+                            <Card.Body>
+                                <Card.Title>{skill.field}</Card.Title>
+                                {Object.entries(skill.list).map(([key, value], index) => (
+                                    Array.isArray(value) ? (
+                                        <div key={index}>
+                                            <strong>{key}:</strong>
+                                            <ListGroup variant="flush">
+                                                {value.map((item, itemIndex) => (
+                                                    <ListGroup.Item key={itemIndex}>{item}</ListGroup.Item>
+                                                ))}
+                                            </ListGroup>
                                         </div>
-                                    ))}
-                                </div>
-                            ) : (
-                                <p key={index}>
-                                    <strong>{key}:</strong> {value}
-                                </p>
-                            ))
-                        ))}
-                    </div>
-                ))}
-            </section>
+                                    ) : (typeof value === 'object' ? (
+                                        <div key={index}>
+                                            <strong>{key}:</strong>
+                                            {Object.entries(value).map(([subKey, subValue], subIndex) => (
+                                                <div key={subIndex}>
+                                                    <strong>{subKey}:</strong>
+                                                    <ListGroup variant="flush">
+                                                        {Array.isArray(subValue) ? (
+                                                            subValue.map((subItem, subItemIndex) => (
+                                                                <ListGroup.Item key={subItemIndex}>{subItem}</ListGroup.Item>
+                                                            ))
+                                                        ) : (
+                                                            <ListGroup.Item>{subValue}</ListGroup.Item>
+                                                        )}
+                                                    </ListGroup>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    ) : (
+                                        <Card.Text key={index}>
+                                            <strong>{key}:</strong> {value}
+                                        </Card.Text>
+                                    ))
+                                ))}
+                            </Card.Body>
+                        </Card>
+                    ))}
+                </section>
 
-            <section>
-                <h2>Awards & Honors</h2>
-                {awardsAndHonors.map((item, index) => (
-                    <div key={index} style={{ marginBottom: "20px" }}>
-                        <p><strong>{item.name}</strong></p>
-                        <p>{item.unit} · {item.time}</p>
-                        <p dangerouslySetInnerHTML={{ __html: item.desc }} />
-                    </div>
-                ))}
-            </section>
+                {/* Awards & Honors Section */}
+                <section>
+                    <h2>Awards & Honors</h2>
+                    {awardsAndHonors.map((item, index) => (
+                        <Card key={index} className="mb-4">
+                            <Card.Body>
+                                <Card.Text><strong>{item.name}</strong></Card.Text>
+                                <Card.Text>{item.unit} · {item.time}</Card.Text>
+                                <Card.Text dangerouslySetInnerHTML={{ __html: item.desc }} />
+                            </Card.Body>
+                        </Card>
+                    ))}
+                </section>
+            </Container>
         </div>
-    )
+    );
 }
 
 export default Experience;
